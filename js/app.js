01495142d546ef6a5e039b85d51af24dfd12b30c
Rees Draminski <rees.draminski@gmail.com>
2020-05-30 08:31:55 -0400

    Add another problem type

--- js/app.js
+++ js/app.js
@@ -1,26 +1,131 @@
+const editor = CodeMirror(document.getElementById("editor"), {
+    mode: { name: "javascript" },
+    lineNumbers: true,
+    indentWithTabs: true,
+    indentUnit: 4,
+    lineWrapping: true
+});
+
 /**
- * Set the code of the prompt text code view element.
+ * Set the contents of the code prompt view to formatted code text.
  * @param {String} code 
  */
-function setPromptText(code) {
-    // create code block as pre to keep whitespace
-    const el = document.getElementById("code_prompt");
+function setPromptCode(code) {
+    const exampleEl = document.getElementById("example");
 
     // clear out anything that could still be in the code prompt element
-    el.innerHTML = "";
+    exampleEl.innerHTML = "";
 
     // add code mirror className so syntax highlighting works
-    el.className = "cm-s-default";
+    exampleEl.className = "cm-s-default";
 
     // run CodeMirror syntax highlighting on the code
-    CodeMirror.runMode(code, { name: "javascript" }, el);
+    CodeMirror.runMode(code, { name: "javascript" }, example);
+}
+
+/**
+ * Select and return a random element from an array.
+ * @param {Array} arr
+ * @returns {Any} element
+ */
+function selectRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+/**
+ * Checkd the code to see if it has valid syntax.
+ * @param {String} code 
+ * @returns {Boolean} isValidSyntax
+ */
+function checkCode(code) {
+    try {
+        esprima.parse(code);
+    } 
+    catch(e) {
+        return false;
+    }
+    
+    return true;
+}
+
+/**
+ * Check the user's answer.
+ * @param {String} promptText 
+ * @param {String} response 
+ */
+function answerPrompt(promptText, response) {
+    // if the code syntax is correct
+    if (checkCode(promptText)) {
+        if (response == "correct") {
+            console.log("You right.");
+        }
+        else {
+            console.log("You wrong.");
+        }
+    }
+    // if the problem syntax is incorrect
+    else {
+        if (response == "correct") {
+            console.log("You wrong.");
+        }
+        else {
+            document.getElementById("editor").style.display = "";
+            editor.refresh();
+        }
+    }
+}
+
+/**
+ * Generate a code example to display to the user as a syntax problem.
+ * @returns {String} problemText
+ */
+function generateProblem() {
+    const problemTypes = ["forLoop", "stringClosure"];
+
+    let problemType = selectRandom(problemTypes);
+    let problemText;
+
+    if (problemType == "forLoop") {
+        const variants = ["missingFirstSemi", "missingSecondSemi", "correct"];
+        let variant = selectRandom(variants);
+
+        if (variant == "missingFirstSemi") {
+            problemText = "for (let x = 0 x < 5; x++) {\n\n}";
+        }
+        else if (variant == "missingSecondSemi") {
+            problemText = "for (let x = 0; x < 5 x++) {\n\n}";
+        }
+        else if (variant == "correct") {
+            problemText = "for (let x = 0; x < 5; x++) {\n\n}";
+        }
+    }
+    else if (problemType == "stringClosure") {
+        const firstQuote  = Math.round(Math.random()) ? "\"" : "'";
+        const secondQuote = Math.round(Math.random()) ? "\"" : "'";
+        const text = Math.random().toString(36).replace(/[^a-z]+/g, '');
+
+        problemText = `${firstQuote}${text}${secondQuote}`;
+    }
+
+    return problemText;
 }
 
 /**
  * Initialize the UI components.
  */
 (function init() {
-    const code = "'Test'";
+    let promptText = generateProblem();
 
-    setPromptText(code);
+    let state = "ANSWERING_PROMPT";
+
+    document.getElementById("correct").onclick = () => {
+        answerPrompt(promptText, "correct");
+    }
+    
+    document.getElementById("incorrect").onclick = () => {
+        answerPrompt(promptText, "incorrect");
+    }
+
+    // setExample(promptText);
+    setPromptCode(promptText);
 })();
